2736b556993a5a8b7adc62ca3abf2f8a5213990f
manhmoon <manhnn@solarapp.asia>
2021-09-22 09:54:26 +0700

    Initial Commit

---
+++ DemoLivePhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoLivePhoto
+//
+//  Created by Manh Nguyen Ngoc on 22/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
